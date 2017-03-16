7e65befa8fb8066be5fd0cb693890ee61884a2d1
mattiasrosberg <mattias.rosberg@jayway.com>
2017-03-16 15:29:22 +0100

    Adding project

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express')
+var app = express()
+var monitor = require('./monitor.js');
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    monitor.fetchAlarmHistory().then(function (alarmHistory) {
+        var alarmHistoryParagraphs = "";
+        var a = 0;
+        for (a = 0; a < alarmHistory.length; a++) {
+            var hasState = JSON.parse(JSON.parse(JSON.stringify(alarmHistory[a])).HistoryData).newState !== undefined;
+            var color = !hasState || 'OK' === JSON.parse(JSON.parse(JSON.stringify(alarmHistory[a])).HistoryData).newState.stateValue ? 'green' : 'red';
+            var row = "<p style=\"color: " + color + "; padding: 10px\">" + alarmHistory[a].Timestamp.toString().replace("GMT+0000 ", "") + "    " + alarmHistory[a].HistorySummary + "</p>";
+            alarmHistoryParagraphs += row;
+        }
+
+        monitor.performHealthCheck().then(function (result) {
+            var color = result[0].value.statusCode === 200 ? 'green' : 'red';
+            var html = "<html>\n" +
+                "<div style=\"width: auto\">" +
+                "<div style=\"float: left; width: 600px\">" +
+
+            "<div style=\"background-color: #f7f7f7; border: 1px solid #d3d3d3; border-radius: 10px; padding-left: 20px; padding-right: 20px\">\n" +
+            "<p style=\"color:" + color + "\">CMS status STAGE: " + result[0].value.statusCode + "   " + result[0].value.statusMessage + "</p>\n" +
+            "</div>\n" +
+
+            "<div style=\"background-color: #f7f7f7; border: 1px solid #d3d3d3; margin-top: 10px; border-radius: 10px; padding-left: 20px\">\n" +
+            alarmHistoryParagraphs +
+            "</div>\n" +
+            "</div>\n" +
+            "<div style=\"float: left; margin-left: 10px; width: 600px\">" +
+            "<div style=\"background-color: #f7f7f7; border: 1px solid #d3d3d3; border-radius: 10px; padding-left: 20px; padding-right: 20px\">\n" +
+            "<p style=\"color:" + color + "\">API status STAGE: " + result[1].value.statusCode + "</p>\n" +
+            "</div>\n" +
+                "<div style=\"background-color: #f7f7f7; border: 1px solid #d3d3d3; margin-top: 10px; border-radius: 10px; padding-left: 20px\">\n" +
+                "<br></br>\n" +
+                "</div>\n" +
+            "</div>\n" +
+            "</div>\n" +
+            "</html>";
+
+            console.log("HTML: " + html);
+
+            res.send(html);
+        })
+    }).catch(function (error) {
+        console.log('Error: ' + error);
+        res.send("Error: " + JSON.stringify(error));
+    });
+
+
+})
+
+
+app.listen(port, function () {
+    console.log('Started app listening on port ' + port + '!');
+})
---
+++ monitor.js
@@ -0,0 +1,529 @@
+var AWS = require('aws-sdk');
+var Q = require('q');
+var _ = require('underscore');
+var http = require('http');
+var https = require('https');
+var moment = require('moment');
+var rp = require('request-promise');
+
+
+var accessKeyId = process.env.ACCESS_KEY_ID;
+var secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+//var elasticbeanstalk = new AWS.ElasticBeanstalk({
+//    apiVersion: '2012-06-01',
+//    region: 'eu-west-1',
+//    accessKeyId: accessKeyId,
+//    secretAccessKey: secretAccessKey
+//});
+
+var elb = new AWS.ELB({
+    apiVersion: '2012-06-01',
+    region: 'eu-west-1',
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey
+});
+
+//var cloudformation = new AWS.CloudFormation({
+//    apiVersion: '2010-05-15',
+//    region: 'eu-west-1',
+//    accessKeyId: accessKeyId,
+//    secretAccessKey: secretAccessKey
+//});
+
+var cloudWatch = new AWS.CloudWatch({
+    apiVersion: '2010-08-01',
+    region: 'eu-west-1',
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey
+});
+
+var ec2 = new AWS.EC2({
+    apiVersion: '2016-11-15',
+    region: 'eu-west-1',
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey
+})
+
+function cmsEC2Instance() {
+    var deferred = Q.defer();
+    var params = {
+        //ApplicationName: "appTrendsStageTest",
+        //VersionLabels: [
+        //	"app-trends-ui-20160511-1126"
+        //]
+    };
+
+    ec2.describeInstances(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+            deferred.reject(err);
+        }
+        else {
+            //console.log("Data: " + JSON.stringify(data));
+            _.forEach(data.Reservations, function (reservation) {
+                _.forEach(reservation.Instances, function (instance) {
+                    _.forEach(instance.Tags, function (tag) {
+                        if (tag.Key === 'Name' && tag.Value.indexOf('erab-Cms') != -1) {
+                            deferred.resolve(instance.InstanceId);
+                        }
+                    });
+                });
+            });
+
+        }
+    });
+    return deferred.promise;
+}
+
+function elbWithInstance(instanceId) {
+    var deferred = Q.defer();
+    var params = {
+        //StackName: stackName, /* required */
+        //NextToken: undefined
+    };
+
+    elb.describeLoadBalancers(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+            deferred.reject(error);
+        }
+        else {
+            //console.log("Data: " + JSON.stringify(data));
+            _.forEach(data.LoadBalancerDescriptions, function (loadBalancerDesc) {
+                _.forEach(loadBalancerDesc.Instances, function (instance) {
+                    console.log("Instance in ELB: " + instance.InstanceId);
+                    if (instance.InstanceId === instanceId) {
+                        deferred.resolve(loadBalancerDesc);
+                    }
+                });
+            });
+
+        }
+    });
+    return deferred.promise;
+}
+
+function performHealthCheckCMS() {
+    var deferred = Q.defer();
+    console.log("Performing healthcheck CMS!!!");
+    cmsEC2Instance().then(function (instanceId) {
+        console.log("InstanceId: " + JSON.stringify(instanceId));
+        elbWithInstance(instanceId).then(function (elb) {
+            console.log("ELB: " + JSON.stringify(elb));
+
+            var options = {
+                host: elb.DNSName,
+                //host: 'www.google.se',
+                path: '/',
+                port: 443,
+                method: 'GET',
+                rejectUnauthorized: false,
+                requestCert: true,
+                agent: false
+            };
+
+            console.log("URL: " + options.host);
+
+            var req = https.request(options, function (response) {
+                console.log("Done with CMS Healthcheck. " + response.statusCode + " " + response.statusMessage);
+                deferred.resolve({
+                    statusCode: response.statusCode,
+                    statusMessage: response.statusMessage,
+                });
+
+            });
+            req.end();
+
+        }).catch(function (error) {
+            console.log("Error: " + JSON.stringify(error));
+            deferred.reject(error);
+        });
+    });
+    return deferred.promise;
+}
+
+function performHealthCheckAPI() {
+    var deferred = Q.defer();
+    console.log("Performing healthcheck API!!!");
+
+    var options = {
+        method: 'GET',
+        uri: 'https://api.erabu.sonymobile.com/info',
+        resolveWithFullResponse: true
+    };
+
+    rp(options).then(function (response) {
+            console.log("Done with API Healthcheck. " + response.statusCode);
+            deferred.resolve({
+                statusCode: response.statusCode,
+            });
+        })
+        .catch(function (err) {
+            console.log("API Error: " + JSON.stringify(err));
+        });
+
+    return deferred.promise;
+}
+
+function performHealthCheck() {
+    var deferred = Q.defer();
+    console.log("Performing healthcheck!!!");
+
+    var promises = [];
+    promises.push(performHealthCheckCMS());
+    promises.push(performHealthCheckAPI());
+    Q.allSettled(promises).then(function( results){
+        console.log("All here: " + JSON.stringify(results));
+        deferred.resolve(results);
+    });
+
+    return deferred.promise;
+}
+
+function getCMSAlarm() {
+    var deferred = Q.defer();
+    var params = {
+        //ActionPrefix: 'STRING_VALUE',
+        //AlarmNamePrefix: 'STRING_VALUE',
+        //AlarmNames: [
+        //    'STRING_VALUE',
+        //    /* more items */
+        //],
+        //MaxRecords: 0,
+        //NextToken: 'STRING_VALUE',
+        //StateValue: 'OK | ALARM | INSUFFICIENT_DATA'
+    };
+    cloudWatch.describeAlarms(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            deferred.reject(err);
+        } // an error occurred
+        else {
+            //console.log(JSON.stringify(data));
+            _.forEach(data.MetricAlarms, function (alarm) {
+                //console.log("Alarm name: " + alarm.AlarmName);
+                if (alarm.AlarmName === 'cmsHealthyHostCountAlarm') {
+                    console.log("Resolving Alarm");
+                    deferred.resolve(alarm);
+                }
+            });
+        }
+        deferred.reject(err);
+    });
+
+    return deferred.promise;
+}
+
+function getAlarmHistory(alarm) {
+    var deferred = Q.defer();
+
+    //var params = {
+    //    AlarmName: alarm.AlarmName,
+    //    EndDate: new Date,
+    //    HistoryItemType: 'ConfigurationUpdate|StateUpdate|Action',
+    //    MaxRecords: 100,
+    //    //NextToken: 'STRING_VALUE',
+    //    StartDate: new Date(moment().add(-2, 'weeks').toDate())
+    //};
+
+    var params = {
+        AlarmName: alarm.AlarmName,
+        EndDate: new Date || 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789,
+        //HistoryItemType: 'StateUpdate | Action | ConfigurationUpdate',
+        MaxRecords: 100,
+        //NextToken: 'STRING_VALUE',
+        StartDate: new Date(moment().add(-2, 'weeks').toDate())
+    };
+
+    cloudWatch.describeAlarmHistory(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            deferred.reject(err);
+        } // an error occurred
+        else {
+            //console.log("Resolving AlarmHistory: " + JSON.stringify(data));
+            var alarmHistory = [];
+            _.forEach(data.AlarmHistoryItems, function (alarmHistoryItem) {
+                if (alarmHistoryItem.HistoryItemType !== 'Action') {
+                    alarmHistory.push(alarmHistoryItem);
+                }
+            })
+            deferred.resolve(alarmHistory);
+        }
+    });
+    return deferred.promise;
+}
+
+function fetchAlarmHistory() {
+    var deferred = Q.defer();
+    getCMSAlarm().then(function (alarm) {
+        //console.log(JSON.stringify(alarm));
+        getAlarmHistory(alarm).then(function (alarmHistory) {
+            deferred.resolve(alarmHistory);
+        })
+    }).catch(function (error) {
+        console.log("Error fetching Alarm and/or AlarmHistory: " + JSON.stringify(error));
+        deferred.resolve(error);
+    });
+
+    return deferred.promise;
+}
+
+fetchAlarmHistory();
+
+exports.performHealthCheck = performHealthCheck;
+exports.fetchAlarmHistory = fetchAlarmHistory;
+
+//function cfStackInfo() {
+//    var deferred = Q.defer();
+//    var params = {
+//        //ApplicationName: "appTrendsStageTest",
+//        //VersionLabels: [
+//        //	"app-trends-ui-20160511-1126"
+//        //]
+//    };
+//
+//    cloudformation.listStacks(params, function (err, data) {
+//        if (err) {
+//            console.log(err, err.stack); // an error occurred
+//            deferred.reject(error);
+//        }
+//        else {
+//            //console.log("Data: " + JSON.stringify(data));
+//            _.forEach(data.StackSummaries, function (stackSummary) {
+//                if (stackSummary.StackName.indexOf('erabu-dev-infra-db-1-cms') != -1 && stackSummary.DeletionTime == undefined) {
+//                    console.log("StackName: " + stackSummary.StackName + "      " + stackSummary.StackId + "  " + stackSummary.CreationTime + "   " + stackSummary.DeletionTime);
+//                    deferred.resolve({
+//                        stackSummary: stackSummary,
+//                        nextToken: data.NextToken
+//                    });
+//                }
+//            });
+//
+//        }
+//    });
+//    return deferred.promise;
+//}
+
+//function cfStackResources(stackName, nextToken) {
+//    var deferred = Q.defer();
+//    var params = {
+//        StackName: stackName, /* required */
+//        NextToken: undefined
+//    };
+//
+//    cloudformation.listStackResources(params, function (err, data) {
+//        if (err) {
+//            console.log(err, err.stack); // an error occurred
+//            deferred.reject(error);
+//        }
+//        else {
+//            //console.log("Data: " + JSON.stringify(data));
+//            //_.forEach(data.StackSummaries, function(stackSummary){
+//            //    if(stackSummary.StackName.indexOf('erabu-dev-infra-db-1-cms') != -1 && stackSummary.DeletionTime == undefined) {
+//            //        console.log("StackName: " + stackSummary.StackName + "      " + stackSummary.StackId + "  " + stackSummary.CreationTime + "   " + stackSummary.DeletionTime);
+//            //        deferred.resolve({
+//            //            stackSummary: stackSummary,
+//            //            nextToken: data.NextToken
+//            //        });
+//            //    }
+//            //});
+//
+//        }
+//    });
+//    return deferred.promise;
+//}
+
+//function performHealthCheck() {
+//    var deferred = Q.defer();
+//
+//    var options = {
+//        host: 'www.google.se',
+//        path: '/',
+//        port: 443,
+//        method: 'GET',
+//        rejectUnauthorized: false,
+//        requestCert: true,
+//        agent: false
+//    };
+//
+//    console.log("URL: " + options.host);
+//
+//    var req = https.request(options, function (response) {
+//        console.log(response.statusCode + " " + response.statusMessage);
+//        deferred.resolve(response.statusCode + " " + response.statusMessage);
+//
+//    });
+//    req.end();
+//
+//    return deferred.promise;
+//}
+
+//performHealthCheck();
+
+//listELBs().then(function(list){
+//    console.log("List: " + JSON.stringify(list));
+//});
+
+//function beanstalkInfo() {
+//    var deferred = Q.defer();
+//    var params = {
+//        //ApplicationName: "appTrendsStageTest",
+//        //VersionLabels: [
+//        //	"app-trends-ui-20160511-1126"
+//        //]
+//    };
+//
+//    elasticbeanstalk.describeApplications(params, function (err, data) {
+//        if (err) {
+//            console.log(err, err.stack); // an error occurred
+//            deferred.reject(err);
+//        }
+//        else {
+//            console.log("Resolve here");
+//            deferred.resolve(data); // successful response
+//        }
+//    });
+//
+//    return deferred.promise;
+//}
+
+//function testIt() {
+//    elasticbeanstalk.describeApplications(params, function (err, data) {
+//        if (err) {
+//            console.log(err, err.stack); // an error occurred
+//        }
+//        else {
+//            console.log("Resolve here");
+//            return 5;
+//        }
+//    });
+//}
+
+//function testIt() {
+//    console.log("Starting here");
+//    var promises = [];
+//    promises.push(beanstalkInfo());
+//    Q.allSettled(promises).then(function (data) {
+//        console.log("Data: " + JSON.stringify(data));
+//        return 5;
+//    }).catch(function (error) {
+//        console.log("Error: " + JSON.stringify(error));
+//    });
+//    //setTimeout(function apa(){
+//    //    console.log("Apa");
+//    //}, 3000);
+//}
+
+//testIt();
+
+//exports.handler = function (event, context, callback) {
+//    console.log("Starting here");
+//    var params = {
+//        //ApplicationName: "appTrendsStageTest",
+//        //VersionLabels: [
+//        //	"app-trends-ui-20160511-1126"
+//        //]
+//    }
+//
+//    elasticbeanstalk.describeApplications(params, function (err, data) {
+//        if (err) {
+//            console.log(err, err.stack); // an error occurred
+//            //deferred.reject(err);
+//        }
+//        else {
+//            console.log("Resolve here");
+//            //deferred.resolve(data); // successful response
+//        }
+//    });
+//    console.log("Done with this Lambda");
+//}
+
+//var params = {
+//	AttributeNames: [
+//		'All'
+//		//'Status | Color | Causes | ApplicationMetrics | InstancesHealth | All | HealthStatus | RefreshedAt',
+//		/* more items */
+//	],
+//	EnvironmentId: 'e-jrftdc4md6',
+//	EnvironmentName: 'erabu-dev-infra-db-1-cms-16-CmsApplication-SOOESHD5O2BI'
+//};
+//elasticbeanstalk.describeEnvironmentHealth(params, function(err, data) {
+//	if (err) console.log(err, err.stack); // an error occurred
+//	else     console.log(data);           // successful response
+//});
+
+//var params = {
+//	EnvironmentName: 'erabu-dev-infra-db-1-cms-16-CmsApplication-SOOESHD5O2BI',
+//	EndTime: new Date()
+//};
+//elasticbeanstalk.describeEvents(params, function(err, data) {
+//	if (err) console.log(err, err.stack); // an error occurred
+//	else     console.log(data);           // successful response
+//	/*
+//	 data = {
+//	 Events: [
+//	 {
+//	 ApplicationName: "my-app",
+//	 EnvironmentName: "my-env",
+//	 EventDate: <Date Representation>,
+//	 Message: "Environment health has transitioned from Info to Ok.",
+//	 Severity: "INFO"
+//	 },
+//	 {
+//	 ApplicationName: "my-app",
+//	 EnvironmentName: "my-env",
+//	 EventDate: <Date Representation>,
+//	 Message: "Environment update completed successfully.",
+//	 RequestId: "b7f3960b-4709-11e5-ba1e-07e16200da41",
+//	 Severity: "INFO"
+//	 },
+//	 {
+//	 ApplicationName: "my-app",
+//	 EnvironmentName: "my-env",
+//	 EventDate: <Date Representation>,
+//	 Message: "Using elasticbeanstalk-us-west-2-012445113685 as Amazon S3 storage bucket for environment data.",
+//	 RequestId: "ca8dfbf6-41ef-11e5-988b-651aa638f46b",
+//	 Severity: "INFO"
+//	 },
+//	 {
+//	 ApplicationName: "my-app",
+//	 EnvironmentName: "my-env",
+//	 EventDate: <Date Representation>,
+//	 Message: "createEnvironment is starting.",
+//	 RequestId: "cdfba8f6-41ef-11e5-988b-65638f41aa6b",
+//	 Severity: "INFO"
+//	 }
+//	 ]
+//	 }
+//	 */
+//});
+
+//var params = {
+//	InfoType: 'bundle', /* required */
+//	EnvironmentId: 'e-c2prb7feip',
+//	EnvironmentName: 'appTrendsStageTest-hvm-env'
+//};
+//elasticbeanstalk.retrieveEnvironmentInfo(params, function(err, data) {
+//	if (err) console.log(err, err.stack); // an error occurred
+//	else     console.log(data);           // successful response
+//});
+
+
+//var params = {
+//	LoadBalancerName: 'udl-stage-Frontend-KWCZQ3LL5G8D', /* required */
+//	Instances: [
+//		{
+//			InstanceId: 'i-094b5478d0cd9fe19'
+//		},
+//		/* more items */
+//	]
+//};
+//elb.describeInstanceHealth(params, function(err, data) {
+//	if (err) console.log(err, err.stack); // an error occurred
+//	else {
+//		console.log("ELB Instance Health:>>>>>>>>>>");
+//		console.log(data);           // successful response
+//	}
+//});
